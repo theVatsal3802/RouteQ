8c4cb6b31b5259e4c7a877aa65351e91538a8dcf
Vatsal Adhiya <vatsaladhiya248@gmail.com>
2026-03-15 16:46:26 +0530

    Initial Commit
    Signed-off-by: Vatsal Adhiya <vatsaladhiya248@gmail.com>

---
+++ src/main/java/com/vatsaladhiya/routeq/routeq/RouteQApplication.java
@@ -0,0 +1,13 @@
+package com.vatsaladhiya.routeq.routeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vatsaladhiya/routeq/routeq/RouteQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vatsaladhiya.routeq.routeq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
